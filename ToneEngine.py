3362993fca85dbceda300774fdc2f527194e9c65
liviu <liviu.teris@gmail.com>
2019-04-28 19:39:07 +0200

    Added sound.

--- ToneEngine.py
+++ ToneEngine.py
@@ -6,29 +6,35 @@ import time
 from termcolor import colored
 
 class SampleLib:
-    def __enter__(self):
+    def __init__(self):
         pyglet.options['audio'] = ('openal', 'pulse', 'silent')
-        self.samples = [pyglet.resource.media("samples/Cowbell-1.wav", streaming = False),
-            pyglet.resource.media("samples/Cowbell-2.wav", streaming = False),
-            pyglet.resource.media("samples/Cowbell-3.wav", streaming = False),
-            pyglet.resource.media("samples/Cowbell-4.wav", streaming = False)]
+        self._samples = {4: pyglet.resource.media("samples/Cowbell-1.wav", streaming = False),
+            8: pyglet.resource.media("samples/Cowbell-2.wav", streaming = False),
+            16: pyglet.resource.media("samples/Cowbell-3.wav", streaming = False),
+            32: pyglet.resource.media("samples/Cowbell-4.wav", streaming = False)}
 
-        return self
+        self._warm_samples = {4: pyglet.resource.media("samples/Klank-1.wav", streaming = False),
+            8: pyglet.resource.media("samples/Klank-2.wav", streaming = False),
+            16: pyglet.resource.media("samples/Klank-3.wav", streaming = False),
+            32: pyglet.resource.media("samples/Klank-4.wav", streaming = False)}
 
-    def play(self, division):
-        if 0 <= division < 4:
-            self.samples[division].play()
+        self._first_sample = pyglet.resource.media("samples/Klank-6.wav", streaming = False)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        #do nothing
-        pass
+    def play(self, division, firstInBar, warnEnd):
+        if firstInBar:
+            self._first_sample.play()
+        elif warnEnd:
+            self._warm_samples[division].play()
+        else:
+            self._samples[division].play()
 
 class ToneEngine:
     def __init__(self):
-        pass
+        self._sample_lib = SampleLib()
 
     def doTick(self, note, isFirstInBar, isEnd):
         if note != None:
+            self._sample_lib.play(note, isFirstInBar, isEnd)
             if isFirstInBar:
                 print(colored(str(note) + " " + str(isEnd), 'red'))
             else:
