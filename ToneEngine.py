a1b57a327ff6b2358c7d9c0cd20d994f8b26bd3b
liviu <liviu.teris@gmail.com>
2019-04-30 23:17:04 +0200

    Renamed samples.

--- ToneEngine.py
+++ ToneEngine.py
@@ -8,17 +8,18 @@ from termcolor import colored
 class SampleLib:
     def __init__(self):
         pyglet.options['audio'] = ('openal', 'pulse', 'silent')
-        self._samples = {4: pyglet.resource.media("samples/Cowbell-1.wav", streaming = False),
-            8: pyglet.resource.media("samples/Cowbell-2.wav", streaming = False),
-            16: pyglet.resource.media("samples/Cowbell-3.wav", streaming = False),
-            32: pyglet.resource.media("samples/Cowbell-4.wav", streaming = False)}
+        self._samples = {4: pyglet.resource.media("samples/Normal-4.wav", streaming = False),
+            8: pyglet.resource.media("samples/Normal-8.wav", streaming = False),
+            16: pyglet.resource.media("samples/Normal-16.wav", streaming = False),
+            32: pyglet.resource.media("samples/Normal-32.wav", streaming = False)}
 
-        self._warm_samples = {4: pyglet.resource.media("samples/Klank-1.wav", streaming = False),
-            8: pyglet.resource.media("samples/Klank-2.wav", streaming = False),
-            16: pyglet.resource.media("samples/Klank-3.wav", streaming = False),
-            32: pyglet.resource.media("samples/Klank-4.wav", streaming = False)}
+        self._warm_samples = {4: pyglet.resource.media("samples/Warn-4.wav", streaming = False),
+            8: pyglet.resource.media("samples/Warn-8.wav", streaming = False),
+            16: pyglet.resource.media("samples/Warn-16.wav", streaming = False),
+            32: pyglet.resource.media("samples/Warn-32.wav", streaming = False)}
 
-        self._first_sample = pyglet.resource.media("samples/Klank-6.wav", streaming = False)
+        self._first_sample = pyglet.resource.media("samples/First.wav", streaming = False)
+        self._pause_sample = pyglet.resource.media("samples/Pause.wav", streaming = False)
 
     def play(self, division, firstInBar, warnEnd):
         if firstInBar:
