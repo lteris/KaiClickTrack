7f0f8b9c48545baed8ab2ca924426d14ee144a60
liviu <liviu.teris@gmail.com>
2019-05-01 14:30:55 +0200

    Use pyglet.media.load.

--- ToneEngine.py
+++ ToneEngine.py
@@ -1,6 +1,7 @@
 ''' Generate tick when triggered '''
 from TrackStream import BarTickSound
 import pyglet
+from pyglet.media import Player
 import time
 
 from termcolor import colored
@@ -8,18 +9,19 @@ from termcolor import colored
 class SampleLib:
     def __init__(self):
         pyglet.options['audio'] = ('openal', 'pulse', 'silent')
-        self._samples = {4: pyglet.resource.media("samples/Normal-4.wav", streaming = False),
-            8: pyglet.resource.media("samples/Normal-8.wav", streaming = False),
-            16: pyglet.resource.media("samples/Normal-16.wav", streaming = False),
-            32: pyglet.resource.media("samples/Normal-32.wav", streaming = False)}
+        self._samples = {4: pyglet.media.load("samples/Normal-4.wav", streaming = False),
+            8: pyglet.media.load("samples/Normal-8.wav", streaming = False),
+            16: pyglet.media.load("samples/Normal-16.wav", streaming = False),
+            32: pyglet.media.load("samples/Normal-32.wav", streaming = False)}
 
-        self._warm_samples = {4: pyglet.resource.media("samples/Warn-4.wav", streaming = False),
-            8: pyglet.resource.media("samples/Warn-8.wav", streaming = False),
-            16: pyglet.resource.media("samples/Warn-16.wav", streaming = False),
-            32: pyglet.resource.media("samples/Warn-32.wav", streaming = False)}
+        self._warm_samples = {4: pyglet.media.load("samples/Warn-4.wav", streaming = False),
+            8: pyglet.media.load("samples/Warn-8.wav", streaming = False),
+            16: pyglet.media.load("samples/Warn-16.wav", streaming = False),
+            32: pyglet.media.load("samples/Warn-32.wav", streaming = False)}
 
-        self._first_sample = pyglet.resource.media("samples/First.wav", streaming = False)
-        self._pause_sample = pyglet.resource.media("samples/Pause.wav", streaming = False)
+        self._first_sample = pyglet.media.load("samples/First.wav", streaming = False)
+        self._pause_sample = pyglet.media.load("samples/Pause.wav", streaming = False)
+
 
     def play(self, division, firstInBar, barTick = BarTickSound.NORMAL):
         if firstInBar:
